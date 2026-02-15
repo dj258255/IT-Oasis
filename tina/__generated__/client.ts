638a8165621e85f248df5a920ad074a67065f05a
범수 <dj258255@naver.com>
2026-02-16 03:14:55 +0900

    fix: TinaCMS admin 브라우저 번들에서    Node.js API 제거
       tina/config.ts의 fs/path 정적 import를 typeof window 가드 +
       동적 require로 변경하여 admin 페이지 "process is not defined" 에러 해결

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/beomsu/Desktop/gitblog/tina/__generated__/.cache/1771175295728', url: 'https://content.tinajs.io/2.1/content/99189c13-9144-4d16-b534-fe8af30bcbe9/github/main', token: '79d3c8cb6b05c739683dd457d45c101eea87c341', queries,  });
+export const client = createClient({ cacheDir: '/Users/beomsu/Desktop/gitblog/tina/__generated__/.cache/1771179221038', url: 'https://content.tinajs.io/2.1/content/99189c13-9144-4d16-b534-fe8af30bcbe9/github/main', token: '79d3c8cb6b05c739683dd457d45c101eea87c341', queries,  });
 export default client;
   
